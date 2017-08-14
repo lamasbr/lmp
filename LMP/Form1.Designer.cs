d9ae69ec673530d00af053e738bc86011d6372e9
lamasbr <llamas.jf@gmail.com>
2017-08-14 18:43:50 -0300

    LMP first commit
    Commit inicial do LMP (Lamas Media Player).

---
+++ LMP/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace LMP
+{
+    partial class FMediaPlayer
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FMediaPlayer));
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrior = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnMute = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.lstArquivos = new System.Windows.Forms.ListBox();
+            this.media = new AxWMPLib.AxWindowsMediaPlayer();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.media)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.ImageIndex = 4;
+            this.btnPlay.ImageList = this.imageList1;
+            this.btnPlay.Location = new System.Drawing.Point(12, 217);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(32, 32);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Button-Fast-Forward-icon.png");
+            this.imageList1.Images.SetKeyName(1, "Button-First-icon.png");
+            this.imageList1.Images.SetKeyName(2, "Button-Last-icon.png");
+            this.imageList1.Images.SetKeyName(3, "Button-Pause-icon.png");
+            this.imageList1.Images.SetKeyName(4, "Button-Play-icon.png");
+            this.imageList1.Images.SetKeyName(5, "Button-Rewind-icon.png");
+            this.imageList1.Images.SetKeyName(6, "Button-Stop-icon.png");
+            this.imageList1.Images.SetKeyName(7, "Sound-on-icon.png");
+            this.imageList1.Images.SetKeyName(8, "Sound-off-icon.png");
+            this.imageList1.Images.SetKeyName(9, "Save-icon.png");
+            this.imageList1.Images.SetKeyName(10, "Button-Add-icon.png");
+            this.imageList1.Images.SetKeyName(11, "Button-Delete-icon.png");
+            // 
+            // btnPause
+            // 
+            this.btnPause.ImageIndex = 3;
+            this.btnPause.ImageList = this.imageList1;
+            this.btnPause.Location = new System.Drawing.Point(50, 217);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(32, 32);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.ImageIndex = 6;
+            this.btnStop.ImageList = this.imageList1;
+            this.btnStop.Location = new System.Drawing.Point(88, 217);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(32, 32);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.ImageIndex = 1;
+            this.btnFirst.ImageList = this.imageList1;
+            this.btnFirst.Location = new System.Drawing.Point(126, 217);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(32, 32);
+            this.btnFirst.TabIndex = 3;
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrior
+            // 
+            this.btnPrior.ImageIndex = 5;
+            this.btnPrior.ImageList = this.imageList1;
+            this.btnPrior.Location = new System.Drawing.Point(164, 217);
+            this.btnPrior.Name = "btnPrior";
+            this.btnPrior.Size = new System.Drawing.Size(32, 32);
+            this.btnPrior.TabIndex = 4;
+            this.btnPrior.UseVisualStyleBackColor = true;
+            this.btnPrior.Click += new System.EventHandler(this.btnPrior_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.ImageIndex = 0;
+            this.btnNext.ImageList = this.imageList1;
+            this.btnNext.Location = new System.Drawing.Point(202, 217);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(32, 32);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.ImageIndex = 2;
+            this.btnLast.ImageList = this.imageList1;
+            this.btnLast.Location = new System.Drawing.Point(240, 217);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(32, 32);
+            this.btnLast.TabIndex = 6;
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.ImageIndex = 9;
+            this.btnSave.ImageList = this.imageList1;
+            this.btnSave.Location = new System.Drawing.Point(240, 179);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(32, 32);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnMute
+            // 
+            this.btnMute.ImageIndex = 8;
+            this.btnMute.ImageList = this.imageList1;
+            this.btnMute.Location = new System.Drawing.Point(202, 179);
+            this.btnMute.Name = "btnMute";
+            this.btnMute.Size = new System.Drawing.Size(32, 32);
+            this.btnMute.TabIndex = 8;
+            this.btnMute.UseVisualStyleBackColor = true;
+            this.btnMute.Click += new System.EventHandler(this.btnMute_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.ImageIndex = 10;
+            this.btnAdd.ImageList = this.imageList1;
+            this.btnAdd.Location = new System.Drawing.Point(240, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(32, 32);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.ImageIndex = 11;
+            this.btnDel.ImageList = this.imageList1;
+            this.btnDel.Location = new System.Drawing.Point(240, 50);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(32, 32);
+            this.btnDel.TabIndex = 10;
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(12, 189);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(42, 13);
+            this.lblVolume.TabIndex = 11;
+            this.lblVolume.Text = "Volume";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(57, 179);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(139, 32);
+            this.hScrollBar1.TabIndex = 12;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // lstArquivos
+            // 
+            this.lstArquivos.FormattingEnabled = true;
+            this.lstArquivos.Location = new System.Drawing.Point(12, 12);
+            this.lstArquivos.Name = "lstArquivos";
+            this.lstArquivos.Size = new System.Drawing.Size(222, 160);
+            this.lstArquivos.TabIndex = 13;
+            // 
+            // media
+            // 
+            this.media.Enabled = true;
+            this.media.Location = new System.Drawing.Point(240, 88);
+            this.media.Name = "media";
+            this.media.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("media.OcxState")));
+            this.media.Size = new System.Drawing.Size(32, 23);
+            this.media.TabIndex = 14;
+            this.media.Visible = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "MP3|*.mp3";
+            // 
+            // FMediaPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.media);
+            this.Controls.Add(this.lstArquivos);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnMute);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrior);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "FMediaPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LMP";
+            ((System.ComponentModel.ISupportInitialize)(this.media)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnPrior;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnMute;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.ListBox lstArquivos;
+        private AxWMPLib.AxWindowsMediaPlayer media;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ LMP/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LMP
+{
+    public partial class FMediaPlayer : Form
+    {
+
+        private bool isPaused = false;
+
+        public FMediaPlayer()
+        {
+            InitializeComponent();
+            hScrollBar1.Value = media.settings.volume;
+            this.lerPlayList();
+        }
+
+        private void lerPlayList()
+        {
+            String path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            path = path.Remove(path.LastIndexOf("\\") + 1) + "playlist.txt";
+
+            if (System.IO.File.Exists(path))
+            {
+                String arquivo = System.IO.File.ReadAllText(path);
+                String[] playList = arquivo.Trim().Split('\n');
+                lstArquivos.Items.AddRange(playList);
+            }
+        }
+
+        private void play(int index)
+        {
+            lstArquivos.SelectedIndex = index;
+            media.URL = lstArquivos.SelectedItem.ToString();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (isPaused)
+            {
+                media.Ctlcontrols.play();
+                isPaused = false;
+            } else
+            {
+                if (lstArquivos.Items.Count > 0)
+                {
+                    if(lstArquivos.SelectedIndex != -1)
+                    {
+                        this.play(lstArquivos.SelectedIndex);
+                    }
+                }
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (!isPaused)
+            {
+                media.Ctlcontrols.pause();
+                isPaused = true;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            media.Ctlcontrols.stop();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            if (lstArquivos.Items.Count > 0)
+            {
+                this.play(0);
+            }
+        }
+
+        private void btnPrior_Click(object sender, EventArgs e)
+        {
+            if (lstArquivos.Items.Count > 0)
+            {
+                if (0 <= lstArquivos.SelectedIndex - 1)
+                {
+                    lstArquivos.SelectedIndex--;
+                    this.play(lstArquivos.SelectedIndex);
+                }
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (lstArquivos.Items.Count > 0)
+            {
+                if (lstArquivos.Items.Count > (lstArquivos.SelectedIndex + 1))
+                {
+                    lstArquivos.SelectedIndex++;
+                    this.play(lstArquivos.SelectedIndex);
+                }
+            }
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            if (lstArquivos.Items.Count > 0)
+            {
+                this.play(lstArquivos.Items.Count - 1);
+            }
+        }
+
+        private void btnMute_Click(object sender, EventArgs e)
+        {
+            media.settings.mute = !media.settings.mute;
+
+            if (media.settings.mute)
+            {
+                btnMute.ImageIndex = 7;
+            } else
+            {
+                btnMute.ImageIndex = 8;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lstArquivos.Items.Add(openFileDialog1.FileName);
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Deseja remover o arquivo de audio?", "Remover audio", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                lstArquivos.Items.Remove(lstArquivos.SelectedItem);
+            }
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            media.settings.volume = hScrollBar1.Value;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            String path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            path = path.Remove(path.LastIndexOf("\\") + 1) + "playlist.txt";
+
+            String itens = "";
+            foreach(String i in lstArquivos.Items)
+            {
+                itens = itens + i.ToString() + "\n";
+            }
+
+            System.IO.File.WriteAllText(path, itens);
+
+            MessageBox.Show("Arquivo salvo com sucesso!", "LMP", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
